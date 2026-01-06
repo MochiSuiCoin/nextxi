a777ffa7cbd1bbfeb05b2755daf40e2c0f114c5a
solly88 <paulo.sol72@gmail.com>
2026-01-06 14:30:05 +1100

    Add .gitignore and remove .next from repo

--- .next/dev/server/chunks/ssr/_next-internal_server_app_programmes_page_actions_ef2a3d29.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/programmes/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_programmes_page_actions_ef2a3d29.js.map
--- .next/dev/server/chunks/ssr/_next-internal_server_app_verified_page_actions_771bcb2e.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/verified/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_verified_page_actions_771bcb2e.js.map
